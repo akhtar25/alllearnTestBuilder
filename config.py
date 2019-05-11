7328e0f67cfcc8562a68a28c79f5159d24d8b6c4
parag <parag@alllearn.in>
2019-05-11 16:07:09 +0530

    minor changes in school performance ranking size

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgres://bpcleitmuybslc:3e37d8b9549fe032323c47b9a38018ac27a0f0ad4516e81c2fc7a87dfc5f4451@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d1mrsoqu6qnsmj?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgres://ewzyzkapvqdlmd:9a80347627991e0aa81ae530b5efd2d90ed615fcc939354684005e6ac4b01ac2@ec2-54-235-167-210.compute-1.amazonaws.com:5432/dfovuta8g4jtqr?sslmode=require'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     POSTS_PER_PAGE = 3
