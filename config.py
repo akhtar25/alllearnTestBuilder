6199950e1efba8d3a7aea6fbab470a4cf6b3a97f
deepak-alllearn <deepak@alllearn.in>
2019-08-21 12:53:17 +0530

    commit

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # staging url    
     DATABASE_URL = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = str(DATABASE_URL) + '?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL + '?sslmode=require'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     POSTS_PER_PAGE = 10
