0537a6d6c2b8cae13b5c14744b6fcf844dddcaee
theparagsinha <parag@alllearn.in>
2019-09-25 13:33:52 +0530

    question description col size in question detail column updated to 1000

--- applicationDB.py
+++ applicationDB.py
@@ -212,7 +212,7 @@ class QuestionDetails(db.Model):
     class_val=db.Column(db.Integer,nullable=True)
     subject_id=db.Column(db.ForeignKey('message_detail.msg_id'),nullable=True)
     board_id=db.Column(db.ForeignKey('message_detail.msg_id'),nullable=True)
-    question_description=db.Column(db.String(500),nullable=True)
+    question_description=db.Column(db.String(1000),nullable=True)
     #slideshow_id=db.Column(db.ForeignKey('slide_tracker.slideshow_id'),nullable=True)
     question_type=db.Column(db.String(120),nullable=True)
     reference_link=db.Column(db.String(120),nullable=True)
