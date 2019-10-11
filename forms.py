c5aa74c3d4f59f2d4be2556ca6a4ae9dda67a922
theparagsinha <parag@alllearn.in>
2019-10-11 20:22:44 +0530

    hidden changes to edit profile form

--- forms.py
+++ forms.py
@@ -53,6 +53,8 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    #full_name =StringField('Username', validators=[DataRequired()])
+    #email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
