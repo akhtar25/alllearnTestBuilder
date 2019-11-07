2016239d94c39fe04fa28858ca7a491de140ad93
theparagsinha <parag@alllearn.in>
2019-11-07 12:10:23 +0530

    bug fix in classseccheck

--- app.py
+++ app.py
@@ -129,13 +129,16 @@ def cityList():
 
 def classSecCheck():
     teacherProfile = TeacherProfile.query.filter_by(user_id=current_user.id).first()
-    classSecRow = ClassSection.query.filter_by(school_id=teacherProfile.school_id).all()
-    print(classSecRow)
-    if len(classSecRow)==0:
+    if teacherProfile==None:
         return 'N'
-        print('returning N')
     else:
-        return 'Y'
+        classSecRow = ClassSection.query.filter_by(school_id=teacherProfile.school_id).all()
+        print(classSecRow)
+        if len(classSecRow)==0:
+            print('returning N')
+            return 'N'            
+        else:
+            return 'Y'
 
     
 
