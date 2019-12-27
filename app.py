92cd4193241eb83b87d8a640bc9794fe5a54bc5a
Zaheen Akhtar <zaheen@alllearn.in>
2019-12-27 13:08:11 +0530

    changes

--- app.py
+++ app.py
@@ -3618,7 +3618,7 @@ if __name__=="__main__":
     #app.run(host=os.getenv('IP', '127.0.0.1'), 
     #        port=int(os.getenv('PORT', 8000)))
     app.run(host=os.getenv('IP', '0.0.0.0'), 
-        port=int(os.getenv('PORT', 8000)),
-        ssl_context='adhoc'
+        port=int(os.getenv('PORT', 8000))
+        # ssl_context='adhoc'
         )
     #app.run()
