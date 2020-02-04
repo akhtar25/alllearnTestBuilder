85d3b8680ea7e66ffac0725bf50dac057ac26f15
theparagsinha <parag@alllearn.in>
2020-02-04 18:07:33 +0530

    test changes

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # staging url    
-    DATABASE_URL = os.environ.get('DATABASE_URL')    
+    #DATABASE_URL = os.environ.get('DATABASE_URL')    
+    DATABASE_URL = "postgres://u3oeikbfat3t4k:p3c0fc3ffa5a59d4a2680634fbf7f4c83fef3d8187462d99ac821cb406ba79fbc@ec2-18-211-206-202.compute-1.amazonaws.com:5432/d5u4rapacupf31"
     EMAIL_API_KEY = os.environ.get('EMAIL_API_KEY')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL + '?sslmode=require'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
