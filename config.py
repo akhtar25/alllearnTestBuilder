3182ed9e914faf739a876836e89e3ea8484d31cf
theparagsinha <parag@alllearn.in>
2020-02-20 21:58:24 +0530

    bug in student profile fixed

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # staging url    
-    DATABASE_URL = os.environ.get('DATABASE_URL')        
+    #DATABASE_URL = os.environ.get('DATABASE_URL')  
+    DATABASE_URL = "postgres://uf6as3sigg3bn5:p5203252e9b23d6af79a04f794f38041eed7e7114fbe40776d843cce06258783f@alllearnstaginginstanceid.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d2vtb4it0a1hst"
     EMAIL_API_KEY = os.environ.get('EMAIL_API_KEY')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL + '?sslmode=require'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
