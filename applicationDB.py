568613209c5b2c9c4b63e8bf60b82159cd5274b8
theparagsinha <parag@alllearn.in>
2020-02-26 00:10:52 +0530

    impact linking in progress

--- applicationDB.py
+++ applicationDB.py
@@ -444,7 +444,7 @@ class StudentProfile(db.Model):
     school_adm_number=db.Column(db.String(120),nullable=True)
     profile_picture= db.Column(db.String(500),nullable=True)
     student_unique_id = db.Column(db.String(50),nullable=True) #added to identify each student since if a student is promoted new row will have to be inserted thereby changing the student_id
-    user_id=db.Column(db.ForeignKey('user.id'), nullable=True)
+    user_id=db.Column(db.ForeignKey('user.id'), nullable=True)    
     sponsored_status = db.Column(db.String(1), nullable=True)
     sponsored_amount = db.Column(db.Integer, nullable=True)
     sponsored_on = db.Column(db.DateTime, nullable=True)
