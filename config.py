1a888c1cc72e0179e2a3e9e125cd393d86c33d90
theparagsinha <parag@alllearn.in>
2020-02-29 14:36:03 +0530

    updates to config

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # staging url    
-    #DATABASE_URL = os.environ.get('DATABASE_URL')   
-    DATABASE_URL = "postgres://uf6as3sigg3bn5:p5203252e9b23d6af79a04f794f38041eed7e7114fbe40776d843cce06258783f@alllearnstaginginstanceid.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d2vtb4it0a1hst"   
+    DATABASE_URL = os.environ.get('DATABASE_URL')   
+    #DATABASE_URL = "postgres://uf6as3sigg3bn5:p5203252e9b23d6af79a04f794f38041eed7e7114fbe40776d843cce06258783f@alllearnstaginginstanceid.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d2vtb4it0a1hst"   
     EMAIL_API_KEY = os.environ.get('EMAIL_API_KEY')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL + '?sslmode=require'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
@@ -14,5 +14,7 @@ class Config(object):
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
     GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration")
-    ALLLEARN_INTERNAL_KEY = "1QAZXSW2JJJJ4RFVLI8761919ASDF654ASDF5D5D222"
-    IMPACT_HOST = "http://127.0.0.1:8000"
+    ALLLEARN_INTERNAL_KEY = os.environ.get('ALLLEARN_INTERNAL_KEY')
+    IMPACT_HOST = os.environ.get('IMPACT_HOST')
+    #ALLLEARN_INTERNAL_KEY = "1QAZXSW2JJJJ4RFVLI8761919ASDF654ASDF5D5D222"
+    #IMPACT_HOST = "http://127.0.0.1:8000"
