3dd5f1ebe245d267625a01d5d010faa14b90d7a1
theparagsinha <parag@alllearn.in>
2020-04-16 23:14:01 +0530

    updates to individual student profile for sponsor data done

--- app.py
+++ app.py
@@ -4587,7 +4587,7 @@ def indivStudentProfile():
     amount = request.args.get('amount')
     
     #New updated query
-    studentProfileQuery = "select full_name, email, sponsored_status,phone,sp.school_id as school_id, dob, md.description as gender,class_val, section, "
+    studentProfileQuery = "select full_name, email, sponsored_status,sponsored_on,sponsor_name,phone,sp.school_id as school_id, dob, md.description as gender,class_val, section, "
     studentProfileQuery = studentProfileQuery + "roll_number,school_adm_number,profile_picture,student_id from student_profile sp inner join "
     studentProfileQuery = studentProfileQuery + "class_section cs on sp.class_sec_id= cs.class_sec_id and sp.student_id='"+str(student_id)+"'" 
     studentProfileQuery = studentProfileQuery + "inner join message_detail md on md.msg_id =sp.gender "
