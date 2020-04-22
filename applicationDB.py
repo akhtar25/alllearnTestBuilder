d9feeb89fb4b489f97e35541df561f2fdfedd037
Zaheen Akhtar <zaheen@alllearn.in>
2020-04-22 22:59:14 +0530

    after change in config file

--- applicationDB.py
+++ applicationDB.py
@@ -238,17 +238,6 @@ class BoardClassSubjectBooks(db.Model):
     book_id = db.Column(db.ForeignKey('book_details.book_id'), nullable=True)
     is_archived = db.Column(db.String(1), nullable=True)
     last_modified_date = db.Column(db.DateTime, nullable=True)
-    
-
-class BoardClassSubjectBooks(db.Model):
-    _tablename_ = "board_class_subject_books"
-    bcsb_id = db.Column(db.Integer,primary_key=True)
-    school_id = db.Column(db.ForeignKey('school_profile.school_id'), nullable= False)
-    class_val = db.Column(db.Integer, nullable=True)
-    subject_id = db.Column(db.ForeignKey('message_detail.msg_id'),nullable=True)
-    book_id = db.Column(db.ForeignKey('book_details.book_id'), nullable=True)
-    is_archived = db.Column(db.String(1), nullable=True)
-    last_modified_date = db.Column(db.DateTime, nullable=True)
 
 
 class ChapterDetail(db.Model):
