f1212abc107915309e2eccef51bd7522fcb2614d
Zaheen Akhtar <zaheen@alllearn.in>
2020-06-12 13:45:32 +0530

    removed repetation of school_id in content_detail

--- applicationDB.py
+++ applicationDB.py
@@ -834,7 +834,6 @@ class ContentDetail(db.Model):
     school_id = db.Column(db.ForeignKey('school_profile.school_id'),nullable=True)
     class_val=db.Column(db.String(20),nullable=True)
     subject_id = db.Column(db.ForeignKey('message_detail.msg_id'),nullable=True)
-    school_id = db.Column(db.ForeignKey('school_profile.school_id'),nullable=True)
     is_private = db.Column(db.String(1),nullable=True)
     content_type=db.Column(db.ForeignKey('message_detail.msg_id'),nullable=True)
     last_modified_date=db.Column(db.DateTime)
