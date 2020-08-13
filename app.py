85393c328d5e0d2245ef79e9fd2497686b67bca7
Zaheen Akhtar <zaheen@alllearn.in>
2020-08-13 15:31:30 +0530

    download report

--- app.py
+++ app.py
@@ -8050,8 +8050,13 @@ def downloadTimeTable():
     # fetchTeacher = db.session.execute(text(fetchTeacher)).fetchall()
     board_id = SchoolProfile.query.filter_by(school_id=teacher_id.school_id).first()
     class_sec_ids = ClassSection.query.filter_by(school_id=teacher_id.school_id).all()
-    filepath = 'static/images/'+str(teacher_id.school_id)+str(dt.datetime.now())+'TimeTable.csv'
-    with open(filepath, 'w', newline='') as file:
+    # filepath = 'static/images/'+str(teacher_id.school_id)+str(dt.datetime.now())+'TimeTable.csv'
+    file_name = 'Timetable'+str(teacher_id.school_id)+'.csv'
+    with open('tempdocx/'+file_name, 'w', newline='') as file:
+        client = boto3.client('s3', region_name='ap-south-1')
+        client.upload_file('tempdocx/'+file_name , os.environ.get('S3_BUCKET_NAME'), 'time_table/{}'.format(file_name),ExtraArgs={'ACL':'public-read'})
+        os.remove('tempdocx/'+file_name)
+    
         
         print('file')
         
@@ -8070,7 +8075,6 @@ def downloadTimeTable():
                 writer.writerow([teacher.subject_name,teacher.teacher_name])
                 # os.remove(filepath)
                 print('File path')
-                print(filepath)
             query = "select *from fn_time_table("+str(teacher_id.school_id)+","+str(class_sec_id.class_sec_id)+")"
             print(query)
             timeTableData = db.session.execute(text(query)).fetchall()
@@ -8079,6 +8083,8 @@ def downloadTimeTable():
             for table in timeTableData:
                 writer.writerow([table.period_no,table.monday,table.tuesday,table.wednesday,table.thursday,table.friday,table.saturday])
             writer.writerow(["","","","","","",""])
+    filepath ='https://'+os.environ.get('S3_BUCKET_NAME')+'.s3.ap-south-1.amazonaws.com/time_table/'+file_name
+    print('filepath:'+str(filepath))
     return jsonify([filepath])
 
 @app.route('/allSubjects',methods=['GET','POST'])
