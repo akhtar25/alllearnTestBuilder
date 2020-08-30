16afc2be9e2f4de657ec4adac771295cd206bf3f
theparagsinha <parag@alllearn.in>
2020-08-31 00:47:29 +0530

    bug fixes, room addition, auto indexation complete

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # staging url  
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    #DATABASE_URL = "postgres://uf6as3sigg3bn5:p5203252e9b23d6af79a04f794f38041eed7e7114fbe40776d843cce06258783f@alllearnstaginginstanceid.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d2vtb4it0a1hst"   
+    DATABASE_URL = "postgres://uf6as3sigg3bn5:p5203252e9b23d6af79a04f794f38041eed7e7114fbe40776d843cce06258783f@alllearnstaginginstanceid.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d2vtb4it0a1hst"   
     # DATABASE_URL = "postgres://u3oeikbfat3t4k:p3c0fc3ffa5a59d4a2680634fbf7f4c83fef3d8187462d99ac821cb406ba79fbc@d5u4rapacupf31.c8et4fo3vjlv.ap-south-1.rds.amazonaws.com/d5u4rapacupf31"
     EMAIL_API_KEY = os.environ.get('EMAIL_API_KEY')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL + '?sslmode=require'
@@ -29,6 +29,8 @@ class Config(object):
     ALLLEARN_CASHFREE_APP_ID = "132537e6d093183e75027048d35231"
     ALLLEARN_CASHFREE_SECRET_KEY = "c44dc87ea6b5c2c8b8dd2ac447f37cafea6264df"
 
+    CASHFREE_API_TEST = "https://ces-gamma.cashfree.com"
+    CASHFREE_API_PROD = "https://ces-api.cashfree.com"
     #ALLLEARN_INTERNAL_KEY = "1QAZXSW2JJJJ4RFVLI8761919ASDF654ASDF5D5D222"
     #IMPACT_HOST = "alllearnimpactstaging.herukoapp.com"
     #IMPACT_HOST = "http://127.0.0.1:8000"
