e8c46728889094acf0f1e8d46ecf019d8ce96eed
theparagsinha <parag@alllearn.in>
2020-09-13 17:07:03 +0530

    tc development 70% complete

--- app.py
+++ app.py
@@ -10045,11 +10045,62 @@ def subscriptionPlans():
     distinctSubsQuery = db.session.execute(text("select distinct group_name, sub_desc, student_limit, teacher_limit, test_limit from subscription_detail where archive_status='N' order by student_limit ")).fetchall()
     return render_template('/subscriptionPlans.html', subscriptionRow=subscriptionRow, distinctSubsQuery=distinctSubsQuery)
 
-@app.route('/studTC')
+@app.route('/studTC',methods=["GET","POST"])
+@login_required
 def studTC():
-    
-    return render_template('studTC.html')
+    teacherData = TeacherProfile.query.filter_by(user_id=current_user.id).first()
+    #tcdata = TransferCerts.query.filter_by(is_archived='N', school_id=teacherData.school_id).all()
+    tcDataQuery = "select sp.student_id, sp.school_adm_number, sp.first_name, sp.last_name, cs.class_val, cs.section, tc.tc_url, tc.tc_id "
+    tcDataQuery = tcDataQuery + " from transfer_certs tc left join student_profile sp on tc.student_id=sp.student_id "
+    tcDataQuery = tcDataQuery + " inner join class_Section cs on cs.class_Sec_id=sp.class_sec_id"
+    tcDataQuery = tcDataQuery + " where tc.is_archived='N' and tc.school_id="+ str(teacherData.school_id)
+    tcData = db.session.execute(tcDataQuery).fetchall()
+    if request.method=="POST":
+        student_id = request.form.get('student_id')
+        school_adm_number = request.form.get('school_adm_number')
+        teacher_id = teacherData.teacher_id
+        school_id = teacherData.school_id
+        tc_url =  request.form.get('pdfURL')
+        #student_id = request.form.get('student_id')
+        chkStudentData = StudentProfile.query.filter_by(student_id=student_id).first()
+        if chkStudentData!=None:
+            transferDataAdd = TransferCerts(student_id=student_id, school_adm_number=school_adm_number, teacher_id=teacher_id
+                ,school_id=school_id, tc_url=tc_url,is_archived='N', last_modified_date=datetime.today())
+            db.session.add(transferDataAdd)
+            db.session.commit()
+            flash('TC Uploaded Successfully!')
+        else:
+            flash(Markup('<span class="red-text"> Student ID invalid. Please try again.</span>'))
+    return render_template('studTC.html',tcData=tcData)
 
+@app.route('/archiveTCClass',methods=["GET","POST"])
+@login_required
+def archiveTCClass():
+    tc_id = request.args.get('tc_id')
+    tcData = TransferCerts.query.filter_by(tc_id=tc_id).first()
+    tcData.is_archived='Y'
+    db.session.commit()
+    return jsonify(['0'])
+
+@app.route('/accessStudTC')
+def accessStudTC():
+    if ("alllearn" in str(request.url)) or  ("localhost" in str(request.url)):        
+        return render_template('accessStudTC.html')
+    else:
+        return jsonify(['Invalid Call'])
+
+@app.route('/fetchStudTC')
+def fetchStudTC():
+    student_id = request.args.get('student_id')
+    school_adm_number = request.args.get('school_adm_number')
+    tcData = TransferCerts.query.filter_by(student_id=student_id,is_archived='N'  ).first()    
+    if tcData!=None:
+        if tcData.tc_url!=None and tcData.tc_url!="":
+            return jsonify([tcData.tc_url])
+        else:
+            return jsonify(['NA'])    
+    else:
+        return jsonify(['NA'])
 
 def format_currency(value):
     return "₹{:,.2f}".format(value)
