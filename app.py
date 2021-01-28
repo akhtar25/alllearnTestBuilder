d49750e84497a70b8229ecc1a3ee39dd57571772
Zaheen Akhtar <zaheen@alllearn.in>
2021-01-28 16:35:01 +0530

    after create api for class list

--- app.py
+++ app.py
@@ -8063,7 +8063,7 @@ def getClassList():
         for classlist in classesListData:
             classList.append(classlist.class_val)
         print(classList)
-        return jsonify(classList)
+        return jsonify({'class_list':classList})
 
 @app.route('/newTestLinkGenerate',methods=['POST'])
 def newTestLinkGenerate():
