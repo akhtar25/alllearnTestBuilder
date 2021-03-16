53e4512c9a42c863237809ea70b840cae2c8da08
Dev Moxaj Desai <dev.moxaj@gmail.com>
2021-03-16 22:23:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12734,6 +12734,7 @@ def format_currency(value):
 if __name__=="__main__":
     app.debug=True  
     #app.use_reloader=False  
+    print("[info] main APP Started")
     app.jinja_env.filters['zip'] = zip
     #app.run(host=os.getenv('IP', '127.0.0.1'), 
     #        port=int(os.getenv('PORT', 8000)))
