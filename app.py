905f876234a2909064510e1be05071e067ce08f5
Zaheen Akhtar <zaheen@alllearn.in>
2021-04-05 19:56:02 +0530

    API for retool

--- app.py
+++ app.py
@@ -14783,7 +14783,7 @@ def addSubjectsforSchool():
         school_id = request.args.get('school_id')
         print('School id:'+str(school_id))
         schoolData = SchoolProfile.query.filter_by(school_id=school_id).first()
-        BCSData = BoardClassSubject.query.filter_by(board_id = schoolData.board_id).first()
+        BCSData = BoardClassSubject.query.filter_by(board_id = schoolData.board_id).all()
         checkDet = BoardClassSubject.query.filter_by(school_id = schoolData.school_id).first()
         if checkDet == None or checkDet == '':
             for data in BCSData:
@@ -14802,7 +14802,7 @@ def addBooksforSchool():
         school_id = request.args.get('school_id')
         print('School id:'+str(school_id))
         schoolData = SchoolProfile.query.filter_by(school_id=school_id).first()
-        BCSBData = BoardClassSubjectBooks.query.filter_by(board_id = schoolData.board_id).first()
+        BCSBData = BoardClassSubjectBooks.query.filter_by(board_id = schoolData.board_id).all()
         checkDet = BoardClassSubjectBooks.query.filter_by(school_id = schoolData.school_id).first()
         if checkDet == None or checkDet == '':
             for data in BCSBData:
